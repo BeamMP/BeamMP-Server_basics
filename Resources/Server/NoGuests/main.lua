aeb8ac80673371fcea9011feffd8ae0b4443380e
Starystars67 <starystars67@gmail.com>
2023-07-12 18:43:07 +0100

    Create NoGuests script

---
+++ Resources/Server/NoGuests/main.lua
@@ -0,0 +1,10 @@
+function onInit()
+	print("NoGuests v1.0-basic Ready")
+	RegisterEvent("onPlayerAuth","onPlayerAuth")
+end
+
+function onPlayerAuth(name, role, isGuest)
+	if isGuest then
+		return "You must be signed in to join this server!"
+	end
+end
